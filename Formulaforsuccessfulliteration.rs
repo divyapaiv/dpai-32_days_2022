f003fb0324502bcd2b5cb0e0c3be186e76d04662
divyapaiv <divyapai993@gmail.com>
2022-09-15 22:57:45 +0530

    32 days of code day 1

---
+++ Formulaforsuccessfulliteration.rs
@@ -0,0 +1,18 @@
+/*Hello cerner_2^5_2022 formula for successfull iteration is always give and take help whenever required 
+Live code can be ran from here https://www.jdoodle.com/ia/vhq 
+*/ 
+fn main() {
+    let give_help = 100/*%*/;
+    let ask_help  = 100/*%*/;
+    let work_completion = give_help / ask_help;
+    if work_completion==1
+       { 
+       println!("Yeyy! No spills in the iteration");
+           
+       }
+    else
+        { 
+        println!("Oh! No we have a spill in the iteration"); 
+            
+        }
+}
