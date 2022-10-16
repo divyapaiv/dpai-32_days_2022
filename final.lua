592cc565e0e2f416353e96ce51d13bb4702bb35a
divyapaiv <divyapai993@gmail.com>
2022-10-16 22:50:32 +0530

    Day 32 - LUA

---
+++ final.lua
@@ -0,0 +1,9 @@
+submission=1 
+finalsubmission=32
+
+ while submission < finalsubmission do
+   print(submission ,"th Submission")
+   submission=submission+1
+end
+print("Finally the last Submission of cerner_2tothe5th_2022 aka Cerner_2^5_2022")
+print("Had a great time while learning and submitting programs. Sayonara!!")
